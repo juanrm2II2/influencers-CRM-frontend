79929abd1ed6a3bfad7171dffd9137b54eb2bb64
Juan Martinez <juanrm2112@gmail.com>
2026-04-25 23:20:26 +0000

    fix issues

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
+import MetaMaskSDK from '@metamask/sdk'
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
   },
 };
 
